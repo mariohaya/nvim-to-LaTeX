b50988bb843bec4d016ee270d242c6cc4b2e4023
mariohaya <152241805+mariohaya@users.noreply.github.com>
2025-05-08 18:45:00 +0800

    Update init.lua
    added -outdir=build parameter

--- init.lua
+++ init.lua
@@ -2,17 +2,20 @@
 -- Set the leader key to backslash (\)
 vim.g.mapleader = "\\"  -- This sets the leader key to backslash
 
--- Define a custom command to compile the current LaTeX file using pdflatex
+-- Define a custom command to compile the current LaTeX file using latexmk
 vim.api.nvim_create_user_command('CompileLatex', function()
   -- Save current file first
   vim.cmd('w')
 
   -- Get the current file's name
   local filename = vim.fn.expand('%')
+
+  -- Create the 'build' directory if it doesn't exist
+  vim.fn.system('mkdir -p build')
   
-  -- Run pdflatex to compile the LaTeX file
-  vim.cmd('!' .. 'pdflatex ' .. filename)
+  -- Run latexmk to compile the LaTeX file into PDF
+  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename)
 end, {})
 
--- Bind the custom command to a key (e.g., <leader>c)
-vim.api.nvim_set_keymap('n', '<leader>c', ':CompileLatex<CR>', { noremap = true, silent = true })
+-- Bind the custom command to a key (e.g., <leader>l)
+vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true })
