b1679f341a32737b19f58f2d538ceb8471896468
mariohaya <152241805+mariohaya@users.noreply.github.com>
2025-05-28 14:50:26 +0800

    Changed to xelatex
    Uses -xelatex now, creates 'build' folder

--- init.lua
+++ init.lua
@@ -1,21 +1,10 @@
------ LaTeX compile
--- Set the leader key to backslash (\)
-vim.g.mapleader = "\\"  -- This sets the leader key to backslash
-
--- Define a custom command to compile the current LaTeX file using latexmk
+----------- LaTeX Compiler -----------
+vim.g.mapleader = "\\"
 vim.api.nvim_create_user_command('CompileLatex', function()
-  -- Save current file first
   vim.cmd('w')
-
-  -- Get the current file's name
   local filename = vim.fn.expand('%')
-
-  -- Create the 'build' directory if it doesn't exist
   vim.fn.system('mkdir -p build')
-  
-  -- Run latexmk to compile the LaTeX file into PDF
-  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename)
+  vim.cmd('!' .. 'latexmk -xelatex -outdir=build ' .. filename)
 end, {})
 
--- Bind the custom command to a key (e.g., <leader>l)
 vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true })
